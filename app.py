b9bd11a3560a32db1514ef6740b79f8a7a896b03
JessicaJuliet <jessica.akomfrah@gmail.com>
2021-05-16 15:17:13 +0000

    Add login functionality

--- app.py
+++ app.py
@@ -56,11 +56,29 @@ def register():
     return render_template("pages/user-authentication.html", register=True)
 
 
-@app.route('/login')
+@app.route('/login', methods=["GET", "POST"])
 def login():
-    """
-    Link to login page
-    """
+    if request.method == "POST":
+        # Check if username exists in database
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            # Ensure hashed password matches user input
+            if check_password_hash(
+                existing_user["password"], request.form.get("password")):
+                    session["user"] = request.form.get("username").lower()
+                    flash("Welcome, {}".format(request.form.get("username")))
+            else:
+                # Invalid password match
+                flash("Incorrect Username and/or Password")
+                return redirect(url_for("login"))
+            
+        else:
+            # Username doesn't exist
+            flash("Incorrect Username and/or Password")
+            return redirect(url_for("login"))
+
     return render_template("pages/user-authentication.html")
 
 
