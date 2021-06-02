4c5fd5a3dd60750c52fef2de32957580e822cc40
JessicaJuliet <jessica.akomfrah@gmail.com>
2021-06-02 19:57:36 +0000

    Update script.js code for date function

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,6 @@
 /* Resource: https://www.w3schools.com/jsref/jsref_tolocaledatestring.asp */
-var date = new Date();
-var n = date.toLocaleDateString();
-document.getElementById("dateToday").innerHTML = n;
+window.addEventListener("DOMContentLoaded", function(){
+    const date = new Date();
+    const dateTodayRef = document.getElementById("dateToday")
+    dateTodayRef.innerHTML = date.toLocaleDateString();
+})
