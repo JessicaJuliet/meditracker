544d686a0a3841d098a883912265ab03d343be2c
JessicaJuliet <jessica.akomfrah@gmail.com>
2021-06-15 21:52:57 +0000

    Set debug to false

--- app.py
+++ app.py
@@ -316,4 +316,4 @@ def linkedin():
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
