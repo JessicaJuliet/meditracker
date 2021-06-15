994661c564e3d22758ccde5f59bc8023ce0195fe
JessicaJuliet <jessica.akomfrah@gmail.com>
2021-06-15 21:43:27 +0000

    update Google font link in css

--- app.py
+++ app.py
@@ -317,5 +317,3 @@ if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
             debug=True)
-# update to debug=False prior to actual
-# deployment of project submission
